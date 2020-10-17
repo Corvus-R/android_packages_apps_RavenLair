84a6079df7d2cbd413fe2065c5d1afb63bf7336f
Ritzz <riteshm321@gmail.com>
2020-10-17 22:02:49 +0200

    Initial bringup for Android 11
    Change-Id: I34a4e3e35189608ef25046fe451e1abfd50b049c

---
+++ src/com/raven/lair/RavenLair.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2014-2016 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.raven.lair;
+
+import android.app.ActionBar;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.viewpager.widget.ViewPager;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
+
+public class RavenLair extends SettingsPreferenceFragment {
+
+    Context mContext;
+    View view;
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CORVUS;
+    }
+}
