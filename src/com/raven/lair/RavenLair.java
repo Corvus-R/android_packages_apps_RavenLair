3c5ce6bb05930d0f047e44cedcd280f0b9f68466
Ritzz <riteshm321@gmail.com>
2020-10-19 23:47:35 +0200

    Initial bringup for Android 11 [2/2]
    Change-Id: Icbfc49a543e73d0ace4644d9d68f2d22a2461a97

--- src/com/raven/lair/RavenLair.java
+++ src/com/raven/lair/RavenLair.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014-2016 The Dirty Unicorns Project
+ * Copyright (C) 2020 CorvusROM
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,6 +36,14 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto;
 
+import com.raven.lair.fragments.team.TeamActivity;
+import com.raven.lair.tabs.Lockscreen;
+import com.raven.lair.tabs.Hardware;
+import com.raven.lair.tabs.Statusbar;
+import com.raven.lair.tabs.System;
+import com.raven.lair.bottomnav.BubbleNavigationConstraintView;
+import com.raven.lair.bottomnav.BubbleNavigationChangeListener;
+
 public class RavenLair extends SettingsPreferenceFragment {
 
     Context mContext;
@@ -44,5 +52,116 @@ public class RavenLair extends SettingsPreferenceFragment {
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.CORVUS;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        mContext = getActivity();
+        View view = inflater.inflate(R.layout.ravenlair, container, false);
+
+        ActionBar actionBar = getActivity().getActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(R.string.ravenlair_title);
+        }
+
+        BubbleNavigationConstraintView bubbleNavigationConstraintView =  (BubbleNavigationConstraintView) view.findViewById(R.id.bottom_navigation_view_constraint);
+        ViewPager viewPager = view.findViewById(R.id.viewpager);
+        PagerAdapter mPagerAdapter = new PagerAdapter(getFragmentManager());
+        viewPager.setAdapter(mPagerAdapter);
+
+        
+        bubbleNavigationConstraintView.setNavigationChangeListener(new BubbleNavigationChangeListener() {
+            @Override
+            public void onNavigationChanged(View view, int position) {
+		    int id = view.getId();
+		    if (id == R.id.system){
+			    viewPager.setCurrentItem(position, true);
+		    }
+		    else if (id ==  R.id.lockscreen){
+		    viewPager.setCurrentItem(position, true);
+		}
+		else if	(id == R.id.statusbar){
+		 viewPager.setCurrentItem(position, true);
+		}
+		else if (id == R.id.hardware) {
+	         viewPager.setCurrentItem(position, true);
+		}
+	      
+                   }
+               }
+           );
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                bubbleNavigationConstraintView.setCurrentActiveItem(i);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+            }
+        });
+
+        setHasOptionsMenu(true);
+        return view;
+    }
+
+    class PagerAdapter extends FragmentPagerAdapter {
+
+        String titles[] = getTitles();
+        private Fragment frags[] = new Fragment[titles.length];
+
+        PagerAdapter(FragmentManager fm) {
+            super(fm);
+            frags[0] = new System();
+            frags[1] = new Lockscreen();
+            frags[2] = new Statusbar();
+            frags[3] = new Hardware();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return frags[position];
+        }
+
+        @Override
+        public int getCount() {
+            return frags.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+    }
+
+    private String[] getTitles() {
+        String titleString[];
+        titleString = new String[]{
+                getString(R.string.bottom_nav_system_title),
+                getString(R.string.bottom_nav_lockscreen_title),
+                getString(R.string.bottom_nav_statusbar_title),
+                getString(R.string.bottom_nav_hardware_title)};
+        return titleString;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, 0, 0, R.string.dialog_team_title);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == 0) {
+            Intent intent = new Intent(mContext, TeamActivity.class);
+            mContext.startActivity(intent);
+            return true;
+        }
+        return false;
     }
 }
---
+++ src/com/raven/lair/fragments/hardware/Buttons.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2017-2019 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.raven.lair.fragments.hardware;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.SearchIndexableResource;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+import androidx.preference.*;
+
+import android.util.Log;
+import android.view.WindowManagerGlobal;
+import android.view.IWindowManager;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.util.corvus.Utils;
+import com.android.settingslib.search.SearchIndexable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import android.text.TextUtils;
+import android.view.View;
+
+@SearchIndexable
+public class Buttons extends SettingsPreferenceFragment {
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CORVUS;
+    }
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<SearchIndexableResource>();
+
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    result.add(sir);
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+                    return keys;
+        }
+    };
+}
