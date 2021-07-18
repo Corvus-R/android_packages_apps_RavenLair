02559788b08e330c0eb674994d1e6fe787125bbf
riteshm321 <ritzz@corvusrom.com>
2021-07-18 21:35:58 +0530

    Lair: Adapt to fod re-impl Credits to neobuddy89

--- src/com/raven/lair/fragments/FingerprintPrefs.java
+++ src/com/raven/lair/fragments/FingerprintPrefs.java
@@ -40,8 +40,12 @@ public class FingerprintPrefs extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener, Indexable {
 
     private static final String FOD_ICON_PICKER_CATEGORY = "fod_icon_picker";
+    private static final String FOD_GESTURE = "fod_gesture";
+    private static final String FOD_NIGHT_LIGHT = "fod_night_light";
 
     private PreferenceCategory mFODIconPickerCategory;
+    private Preference mScreenOffFOD;
+    private Preference mFODnightlight;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -50,8 +54,20 @@ public class FingerprintPrefs extends SettingsPreferenceFragment
         PreferenceScreen prefScreen = getPreferenceScreen();
 
         mFODIconPickerCategory = findPreference(FOD_ICON_PICKER_CATEGORY);
-        if (mFODIconPickerCategory != null && !FodUtils.hasFodSupport(getContext())) {
+        if (!FodUtils.hasFodSupport(getContext())) {
             prefScreen.removePreference(mFODIconPickerCategory);
+        } else {
+            mScreenOffFOD = (Preference) findPreference(FOD_GESTURE);
+            final boolean isScreenOffFodSupported = mContext.getResources().getBoolean(
+                    R.bool.config_supportScreenOffFod);
+            if (!isScreenOffFodSupported) {
+                mFODIconPickerCategory.removePreference(mScreenOffFOD);
+        }
+            mFODnightlight = (Preference) findPreference(FOD_NIGHT_LIGHT);
+            final boolean isFodNightLightSupported = mContext.getResources().getBoolean(
+                    com.android.internal.R.bool.disable_fod_night_light);
+            if (!isFodNightLightSupported) {
+                mFODIconPickerCategory.removePreference(mFODnightlight);
         }
     }
 
